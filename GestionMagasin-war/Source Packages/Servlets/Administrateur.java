4c46bf245b0da2a6651274977457ed1a9f762273
3137574 <3137574@S263-25.ad.univ-lyon3.fr>
2018-03-14 13:45:33 +0100

    Début du dev sur la servlet administrateur

---
+++ GestionMagasin-war/Source Packages/Servlets/Administrateur.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 3137574
+ */
+public class Administrateur extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String jspClient = null;
+        String action = request.getParameter("action");
+        
+        
+        // Jsp par défaut lorsque l'utilisateur exécute la solution
+        if((action==null) || (action.equals("null")))
+        {
+            jspClient="/Menu";
+        }
+        
+        else if(action.equals("creerUtilisateur"))
+        {
+            creerUtilisateur(request, response);
+            jspClient="/creerUtilisateur.jsp";
+        }        
+        
+        RequestDispatcher rd;
+        rd = getServletContext().getRequestDispatcher(jspClient);
+        rd.forward(request, response);
+                
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Administrateur</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Administrateur at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+protected void creerUtilisateur(HttpServletRequest request,
+HttpServletResponse response) throws ServletException, IOException
+{
+    String login = request.getParameter("login");   
+}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
